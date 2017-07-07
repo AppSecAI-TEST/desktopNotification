dc88886d81eda3ebfbc5de4d0953636e2d4207b6
Valmar Junior <valmar.junior1@gmail.com>
2017-07-07 17:44:16 -0300

    Tratamento de exceções

--- src/br/edu/unichristus/main/Main.java
+++ src/br/edu/unichristus/main/Main.java
@@ -1,11 +1,18 @@
 package br.edu.unichristus.main;
 
+import java.awt.AWTException;
+
 import br.edu.unichristus.views.InterfaceGraficaPrincipal;
 
 public class Main {
 
 	public static void main(String[] args) {
-		new InterfaceGraficaPrincipal();
+		try {
+			new InterfaceGraficaPrincipal();
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
--- src/br/edu/unichristus/views/InterfaceGraficaPrincipal.java
+++ src/br/edu/unichristus/views/InterfaceGraficaPrincipal.java
@@ -13,6 +13,7 @@ import java.awt.event.WindowEvent;
 import java.awt.event.WindowStateListener;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -22,10 +23,11 @@ public class InterfaceGraficaPrincipal extends JFrame {
 	private TrayIcon trayIcon;
 	private SystemTray systemTray;
 
-	public InterfaceGraficaPrincipal() {
+	private final Image IMAGE = Toolkit.getDefaultToolkit().getImage("resources/img/printer.png");
+
+	public InterfaceGraficaPrincipal() throws AWTException {
 		super("SystemTray test");
 
-		System.out.println("Settings look and feel");
 		try {
 
 			System.out.println(UIManager.getSystemLookAndFeelClassName());
@@ -40,54 +42,86 @@ public class InterfaceGraficaPrincipal extends JFrame {
 
 		if (SystemTray.isSupported()) {
 
-			System.out.println("isSupported");
-			this.systemTray = SystemTray.getSystemTray();
-
-			Image image = Toolkit.getDefaultToolkit().getImage("resources/printer.png");
-
-			ActionListener exitActionListener = new ActionListener() {
-
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					System.out.println("Saindo");
-					System.exit(0);
-				}
-			};
-
-			PopupMenu popupMenu = new PopupMenu();
-			MenuItem menuItem = new MenuItem("Sair");
-
-			menuItem.addActionListener(exitActionListener);
-			popupMenu.add(menuItem);
-
-			menuItem = new MenuItem("Abrir");
-			menuItem.addActionListener(new ActionListener() {
-
-				@Override
-				public void actionPerformed(ActionEvent arg0) {
-					setVisible(true);
-					setExtendedState(JFrame.NORMAL);
-				}
-			});
-
-			popupMenu.add(menuItem);
-			trayIcon = new TrayIcon(image, "Demo", popupMenu);
-			trayIcon.setImageAutoSize(true);
+			openWindow();
 
 		} else {
 
-			System.out.println("Sysem tray false");
+			// TODO Melhorar esse tratamento de erro
+			throw new AWTException("Não foi possível tratar esse evento");
 
 		}
 
+		minimizeWindowToTray();
+
+		windowPropreties();
+	}
+
+	private void windowPropreties() {
+		setIconImage(IMAGE);
+		setVisible(true);
+		setSize(300, 200);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+
+		addWindowListener(new java.awt.event.WindowAdapter() {
+
+			@Override
+			public void windowClosing(WindowEvent windowEvent) {
+//				if (JOptionPane.showConfirmDialog(frame, message)) {
+//					
+//				}
+			}
+
+		});
+
+	}
+
+	private void openWindow() {
+
+		this.systemTray = SystemTray.getSystemTray();
+
+		ActionListener exitActionListener = new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				System.out.println("Saindo");
+				System.exit(0);
+			}
+		};
+
+		PopupMenu popupMenu = new PopupMenu();
+		MenuItem menuItem = new MenuItem("Sair");
+
+		menuItem.addActionListener(exitActionListener);
+		popupMenu.add(menuItem);
+
+		menuItem = new MenuItem("Abrir");
+		menuItem.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				setVisible(true);
+				setExtendedState(JFrame.NORMAL);
+			}
+		});
+
+		popupMenu.add(menuItem);
+		trayIcon = new TrayIcon(this.IMAGE, "Demo", popupMenu);
+		trayIcon.setImageAutoSize(true);
+
+	}
+
+	private void minimizeWindowToTray() {
 		addWindowStateListener(new WindowStateListener() {
 
 			@Override
 			public void windowStateChanged(WindowEvent windowEvent) {
+				System.out.println(windowEvent.getNewState());
 
 				if (windowEvent.getNewState() == ICONIFIED) {
 					try {
 						systemTray.add(trayIcon);
+						System.out.println("foi pro tray");
 						setVisible(false);
 					} catch (AWTException e) {
 						e.getMessage();
@@ -96,35 +130,27 @@ public class InterfaceGraficaPrincipal extends JFrame {
 
 				if (windowEvent.getNewState() == 7) {
 					try {
+						System.out.println("on tray");
 						systemTray.add(trayIcon);
 						setVisible(false);
-						System.out.println("on tray");
 					} catch (AWTException e) {
 						System.out.println("não consegue né? - Sant");
 					}
 				}
 				if (windowEvent.getNewState() == MAXIMIZED_BOTH) {
-					systemTray.remove(trayIcon);
 					System.out.println("saiu do tray");
+					systemTray.remove(trayIcon);
 				}
 
 				if (windowEvent.getNewState() == NORMAL) {
+					System.out.println("ray sai");
 					systemTray.remove(trayIcon);
 					setVisible(true);
-					System.out.println("ray sai");
 				}
 			}
 		});
-
-		setIconImage(Toolkit.getDefaultToolkit().getImage("resources/printer.png"));
-		windowPropreties();
 	}
 
-	private void windowPropreties() {
-		setVisible(true);
-		setSize(300, 200);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-	}
-
+	// TODO: SEE
+	// https://docs.oracle.com/javase/tutorial/displayCode.html?code=https://docs.oracle.com/javase/tutorial/uiswing/examples/misc/TrayIconDemoProject/src/misc/TrayIconDemo.java
 }
